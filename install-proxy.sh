2a96e565f152f4a6a44124ca01741fd013db89d2
Richard Vencu <vencu.richard@gmail.com>
2023-11-11 23:02:18 +0000

    fix argument name

--- install-proxy.sh
+++ install-proxy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/Stability-AI/iam-credentials-api-proxy /root/iam-credentials-api-proxy
-cp /root/iam-credentials-api-proxy/ica.service /etc/systemd/system/ica.service
+git clone https://github.com/Stability-AI/iam-credentials-api-proxy /opt/iam-credentials-api-proxy
+cp /opt/iam-credentials-api-proxy/ica.service /etc/systemd/system/ica.service
 icahost=$(aws secretsmanager get-secret-value --secret-id "ICAproxyHostname" --query SecretString --output text --region "us-west-2")
 sed -i "s|target_hostname|$icahost|g" /etc/systemd/system/ica.service
 systemctl daemon-reload
